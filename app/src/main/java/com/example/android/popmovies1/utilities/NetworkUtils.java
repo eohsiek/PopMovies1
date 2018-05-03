1e3de82bb774f05f5d1d8e8ef6554d3766ef2131
eohsiek <eohsiek@gmail.com>
2018-05-02 18:25:08 -0600

    testing gitignore

---
+++ app/src/main/java/com/example/android/popmovies1/utilities/NetworkUtils.java
@@ -0,0 +1,9 @@
+package com.example.android.popmovies1.utilities;
+
+public class NetworkUtils {
+
+    final static String MOVIEDB_API_BASE_URI =
+            "http://api.themoviedb.org/3/movie/popular?sort_by=popularity.desc&api_key=2dc2aabd3659e1e1e1c0ebdcd2013082";
+
+    final static String PARAM_QUERY = "q";
+}
