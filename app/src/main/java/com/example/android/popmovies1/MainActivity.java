3f445e208ce7b65b007e48abf18270e6b3337b3d
eohsiek <eohsiek@gmail.com>
2018-05-02 19:49:29 -0600

    working

--- app/src/main/java/com/example/android/popmovies1/MainActivity.java
+++ app/src/main/java/com/example/android/popmovies1/MainActivity.java
@@ -1,13 +1,86 @@
 package com.example.android.popmovies1;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.android.popmovies1.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar mLoadingIndicator;
+    private TextView mResponseView;
+    private TextView mErrorMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.progressBar);
+        mResponseView = (TextView) findViewById(R.id.responseView);
+        mErrorMessage = (TextView) findViewById(R.id.errorMessage);
+        Log.d("myTag", "ApplicationStarted");
+        getMovies();
+    }
+
+    private void getMovies() {
+        URL movieURL = NetworkUtils.buildUrl();
+        new GetPopularMoviesTask().execute(movieURL);
+    }
+
+    public class GetPopularMoviesTask extends AsyncTask<URL, Void, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(URL... params) {
+            URL searchUrl = params[0];
+            String movieResults = null;
+            try {
+                movieResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return movieResults;
+        }
+
+        @Override
+        protected void onPostExecute(String movieResults) {
+            // COMPLETED (27) As soon as the loading is complete, hide the loading indicator
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+            if (movieResults != null && !movieResults.equals("")) {
+                // COMPLETED (17) Call showJsonDataView if we have valid, non-null results
+                showJsonDataView();
+                mResponseView.setText(movieResults);
+            } else {
+                // COMPLETED (16) Call showErrorMessage if the result is null in onPostExecute
+                showErrorMessage();
+            }
+        }
+
+    }
+
+    private void showJsonDataView() {
+        // First, make sure the error is invisible
+        mErrorMessage.setVisibility(View.INVISIBLE);
+        // Then, make sure the JSON data is visible
+        mResponseView.setVisibility(View.VISIBLE);
+    }
+
+    private void showErrorMessage() {
+        // First, hide the currently visible data
+        mErrorMessage.setVisibility(View.INVISIBLE);
+        // Then, show the error
+        mResponseView.setVisibility(View.VISIBLE);
     }
 }
--- app/src/main/java/com/example/android/popmovies1/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/android/popmovies1/utilities/NetworkUtils.java
@@ -1,9 +1,66 @@
 package com.example.android.popmovies1.utilities;
 
+import android.net.Uri;
+import android.util.Log;
+
+import com.example.android.popmovies1.BuildConfig;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class NetworkUtils {
 
     final static String MOVIEDB_API_BASE_URI =
             "http://api.themoviedb.org/3/movie/popular?sort_by=popularity.desc&api_key=2dc2aabd3659e1e1e1c0ebdcd2013082";
+    final static String PARAM_API_KEY = "api_key";
+    final static String API_KEY = BuildConfig.MovieDBAPIKey;
+    final static String PARAM_SORT = "sort_by";
+    final static String SORT_BY = "popularity.desc";
 
-    final static String PARAM_QUERY = "q";
+    public static URL buildUrl() {
+        Uri builtUri = Uri.parse(MOVIEDB_API_BASE_URI).buildUpon()
+                .appendQueryParameter(PARAM_API_KEY, API_KEY)
+                .appendQueryParameter(PARAM_SORT, SORT_BY)
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        Log.d("myTag", "Got the URL");
+
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+
+        try {
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();;
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                return stringBuilder.toString();
+            }
+            finally{
+                urlConnection.disconnect();
+            }
+        }
+        catch(Exception e) {
+            Log.e("ERROR", e.getMessage(), e);
+            return null;
+        }
+    }
 }
