ece6cab1429201404bdb32f3ee08640cd068ad98
eohsiek <eohsiek@gmail.com>
2018-05-11 19:26:18 -0600

    databinding working

--- app/src/main/java/com/example/android/popmovies1/MovieDetail.java
+++ app/src/main/java/com/example/android/popmovies1/MovieDetail.java
@@ -2,12 +2,15 @@ package com.example.android.popmovies1;
 
 import android.content.Context;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.android.popmovies1.data.Movie;
+
+import com.example.android.popmovies1.databinding.ActivityMovieDetailBinding;
 import com.squareup.picasso.Picasso;
 
 public class MovieDetail extends AppCompatActivity {
@@ -15,22 +18,12 @@ public class MovieDetail extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_detail);
-
+        ActivityMovieDetailBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_movie_detail);
 
         Intent intent = getIntent();
         Movie movie = intent.getParcelableExtra("movie");
+        binding.setMovie(movie);
 
-        TextView titleTextView = findViewById(R.id.textview_movie_title);
-        TextView voteAverageTextView = findViewById(R.id.textview_vote_average);
-        TextView overviewTextView = findViewById(R.id.textview_overview);
-        TextView releaseDateTextView = findViewById(R.id.textview_release_date);
-        ImageView posterImageView = findViewById(R.id.imageview_movie_poster);
-
-        titleTextView.setText(movie.getTitle());
-        voteAverageTextView.setText(movie.getVote());
-        releaseDateTextView.setText(movie.getRelease_date());
-        overviewTextView.setText(movie.getOverview());
-        Picasso.with(this).load(movie.getPosterURI()).into(posterImageView);
+        Picasso.with(this).load(movie.getPosterURI()).into(binding.imageviewMoviePoster);
     }
 }
